39af3ca13a95ba3e32a984a98f88a10eb3e0b7d7
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-07-13 23:29:40 +0530

    api data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import NetflixCard from "./components/NetflixCard";
 export default function App() {
-  return <div>App</div>;
+  const [myData, setMyData] = useState([]);
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "44fd12d7e9mshc97fba051aa42bbp1e55c5jsnc2dd769b5228",
+      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+    },
+  };
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        "https://netflix54.p.rapidapi.com/search/?query=stranger&offset=0&limit_titles=50&limit_suggestions=20&lang=en ",
+        options
+      );
+      const data = await res.json();
+      console.log(data);
+      setMyData(data.titles);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div>
+      {/* {myData.map((currele) => {
+        <NetflixCard key={currele.summary.id} actualData={currele} />;
+      })} */}
+    </div>
+  );
 }
---
+++ src/components/NetflixCard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NetflixCard() {
+  return <div>NetflixCard</div>;
+}
