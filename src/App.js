aa9fbb308c6aacb72a10b6af0d018630d41588a8
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-07-17 20:12:47 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import NetflixCard from "./components/NetflixCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 export default function App() {
   const [myData, setMyData] = useState([]);
   const options = {
@@ -28,11 +31,35 @@ export default function App() {
   useEffect(() => {
     fetchData();
   }, []);
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
-    <div>
-      {/* {myData.map((currele) => {
-        <NetflixCard key={currele.summary.id} actualData={currele} />;
-      })} */}
-    </div>
+    <Carousel responsive={responsive} showDots={true}>
+      {myData.map((currentElement) => (
+        <NetflixCard
+          key={currentElement.summary.id}
+          actualData={currentElement}
+        />
+      ))}
+    </Carousel>
   );
 }
